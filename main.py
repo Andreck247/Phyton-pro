373458c4ecaf20b03b4e13d52f7cf737a6d867ef
Andreck247 <anarelis240710@gmail.com>
2025-02-25 03:53:43 -0500

    main.py

--- main.py
+++ main.py
@@ -1,1 +1,14 @@
-print("Hola mundo")
+meme_dict = {
+            "CRINGE": "Algo excepcionalmente raro o embarazoso",
+            "LOL": "Una respuesta común a algo gracioso",
+            "GG": "Se usa para felicitar cuando se juega bien",
+            "IDK": "Se usa para expresar que no se sabe algo",
+            "BTW": "Se utiliza cuando se está hablando sobre un tema concreto y se quiere añadir algo relacionado"
+            }
+word = input("Escribe una palabra que no entiendas (¡con mayúsculas!): ")
+if word in meme_dict.keys():
+    print(meme_dict[word])
+    # ¿Qué debemos hacer si se encuentra la palabra?
+else:
+    print("No tenemos esa palabra en el diccionario")
+    # ¿Qué hacer si no se encuentra la palabra?
